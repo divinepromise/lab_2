496238439fc10cc898106f99a2b7b14d60446192
divinepromise <divinepromise@dal.ca>
2019-05-01 22:46:03 -0700

    task1, first save, extimated square_root

---
+++ task1.py
@@ -0,0 +1,16 @@
+import math
+
+def square_root(a):
+	x=a/2
+	epsilon=0.0000001
+	while True:
+		y=(x+(a/x))/2
+		if abs(y-x) < epsilon:
+			return y
+			break
+		x=y
+
+
+result = square_root(3)
+
+print(result)
