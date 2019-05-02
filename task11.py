ad2ae9d4188ce45535a6ea6275d46963eddada3d
divinepromise <divinepromise@dal.ca>
2019-05-02 03:14:31 -0700

    task11, modified to accept user input and also the list that do not contain forbidden words

--- task11.py
+++ task11.py
@@ -1,10 +1,19 @@
-def avoids(word,letters="aeiou"):
+def avoids(word,letters):
 	for letter in word:
 		if letter in letters:
 			return True
 	return False
 
 
-result = avoids("bbbbbbbbbbbb")
-print(result)
-	
+
+def user_input():
+	forbidden_string = str(input("enter the forbidden strings  "))
+
+	words = open("words.txt")
+
+	for word in words:
+		word=word.strip()
+		if avoids(word,forbidden_string) == False:
+			print (word)
+
+user_input()
