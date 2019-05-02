ba0fe4bfa3e66199e367311c81006c9cf01605cd
divinepromise <divinepromise@dal.ca>
2019-05-02 02:40:56 -0700

    final save of task 10, prints words with no 'e' and also total percentage of them

--- task10_1.py
+++ task10_1.py
@@ -7,10 +7,19 @@ def has_no_e(word):
 
 def word_play2():
 	words = open("words.txt")
-	
+	count1=0
+	count2=0
 	for line in words:
 		line = line.strip()
+		count2 +=1
 		if has_no_e(line) == True:
+			count1 +=1
 			print(line)
+
+	percentage = count1/count2 * 100
+	print("words without 'e' is ",count1)
+	print('Total word count',count2) 
+	print("percentage of occurance of words with no 'e' is  ",int(percentage),"%",sep="" )
+
 
 word_play2()
